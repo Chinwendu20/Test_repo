1b67c3f473f4925c254d646c26be92df3f5e6d82
Chinwendu20 <amaka013@gmail.com>
2021-02-16 23:39:43 +0100

    ""

--- git_auto.sh
+++ git_auto.sh
@@ -1,10 +1,11 @@
+
 #!/bin/bash
 
 while getopts "c:ap:" git
 do
 case "${git}" in
 a) git add .;; 
-c) git commit -m ${OPTARG};;
+c) git commit -m  \"${OPTARG}\";;
 p) git push origin ${OPTARG};;
 esac
 done
